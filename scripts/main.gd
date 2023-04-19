bc312c7b8390c4848a0d9a2f3d2ef3d43699d2be
MrOutsider <dk1ofakind@protonmail.com>
2023-04-19 15:21:41 -0700

    Locked Out Window Base

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var console_outut = %CONSOLE_OUTPUT
 
 var windows : Dictionary = {}
+var LockedAccountsID : int = -1
 var listOfLockedUsers : Dictionary = {}
 
 func _ready():
@@ -62,17 +63,31 @@ func consoleCommandADLocked(cmdArg : PackedStringArray) -> void:
 	
 #	Populate Locked Accounts Dict : listOfLockedUsers
 	if "SamAccountName" in _consoleOutput:
+		listOfLockedUsers.clear()
 		var placeHolderLoop : bool = true
-		var placeHolder : int = 0
-		var placeHolderOffset : int = 0
+#		Username
+		var placeHolderAccountName : int = 0
+		var placeHolderOffsetAccountName : int = 0
+#		Password Expired
+		var placeHolderPasswordExpired : int = 0
+		var placeHolderOffsetPasswordExpired : int = 0
+		
 		var numOfLockedUsers : int = 0
 		while (placeHolderLoop):
-			placeHolder = _consoleOutput.findn("SamAccountName", placeHolder) + 1
-			placeHolderOffset = _consoleOutput.findn("SID", placeHolder) + 1
-			if (placeHolder == 0):
+			placeHolderAccountName = _consoleOutput.findn("SamAccountName", placeHolderAccountName) + 1
+			placeHolderOffsetAccountName = _consoleOutput.findn("SID", placeHolderAccountName) + 1
+			
+			placeHolderPasswordExpired = _consoleOutput.findn("PasswordExpired", placeHolderPasswordExpired) + 1
+			placeHolderOffsetPasswordExpired = _consoleOutput.findn("PasswordNeverExpires", placeHolderPasswordExpired) + 1
+			if (placeHolderAccountName == 0):
 				placeHolderLoop = false
+				var newWin : Window = Window.new()
+				createLockedAccountsWindow(newWin)
 			else:
-				var user : Dictionary = {"user" : _consoleOutput.substr(placeHolder + 23, placeHolderOffset - 25 - placeHolder)}
+				var user : Dictionary = {
+					"SamAccountName" : _consoleOutput.substr(placeHolderAccountName + 23, placeHolderOffsetAccountName - 26 - placeHolderAccountName),
+					"PasswordExpired" : _consoleOutput.substr(placeHolderPasswordExpired + 23 , placeHolderOffsetPasswordExpired - 26 - placeHolderPasswordExpired)
+					}
 				listOfLockedUsers[str(numOfLockedUsers)] = user
 				numOfLockedUsers += 1
 			
@@ -81,6 +96,86 @@ func consoleCommandADLocked(cmdArg : PackedStringArray) -> void:
 		_consoleOutput = _consoleOutput.replacen("PasswordExpired       : True", "[color=darkred]PasswordExpired       : True[/color]")
 		_consoleOutput = _consoleOutput.replacen("PasswordExpired       : False", "[color=darkgreen]PasswordExpired       : False[/color]")
 	addToConsole(_consoleOutput)
+
+func createLockedAccountsWindow(_win : Window) -> void:
+	if (LockedAccountsID != -1):
+		self.get_child(LockedAccountsID).queue_free()
+		await (get_tree().create_timer(0.25).timeout)
+	self.add_child(_win)
+	LockedAccountsID = _win.get_index()
+	windows["Locked_Accounts"] = _win
+	_win.name = "Locked_Accounts"
+	_win.title = "Locked Accounts"
+	_win.position = Vector2(50, 100)
+	_win.min_size = Vector2(500, 100)
+	_win.size = Vector2(500, 200)
+	var closeWin := func() -> void:
+		LockedAccountsID = -1
+		_win.queue_free()
+	_win.close_requested.connect(closeWin)
+	var margin : MarginContainer = MarginContainer.new()
+	_win.add_child(margin)
+	margin.anchors_preset = 15 # PRESET_FULL_RECT
+	var margin_value = 10
+	margin.add_theme_constant_override("margin_top", margin_value)
+	margin.add_theme_constant_override("margin_left", margin_value)
+	margin.add_theme_constant_override("margin_bottom", margin_value)
+	margin.add_theme_constant_override("margin_right", margin_value)
+	var scroll : ScrollContainer = ScrollContainer.new()
+	margin.add_child(scroll)
+	scroll.anchors_preset = 15 # PRESET_FULL_RECT
+	var hBox : HBoxContainer = HBoxContainer.new()
+	scroll.add_child(hBox)
+	hBox.anchors_preset = 15 # PRESET_FULL_RECT
+	hBox.add_theme_constant_override("separation", 20)
+	var vBoxUser : VBoxContainer = VBoxContainer.new()
+	var vBoxPass : VBoxContainer = VBoxContainer.new()
+	hBox.add_child(vBoxUser)
+	hBox.add_child(vBoxPass)
+	vBoxUser.add_theme_constant_override("separation", 10)
+	vBoxPass.add_theme_constant_override("separation", 10)
+	vBoxUser.anchors_preset = 15 # PRESET_FULL_RECT
+	vBoxPass.anchors_preset = 15 # PRESET_FULL_RECT
+	var itemUsernameTitle : Label = Label.new()
+	vBoxUser.add_child(itemUsernameTitle)
+	itemUsernameTitle.text = "Usernames"
+	var itemLockedTitle : Label = Label.new()
+	var vBoxPassTitle : Label = Label.new()
+	vBoxPass.add_child(vBoxPassTitle)
+	vBoxPassTitle.text = "Password Expired"
+		
+	for users in listOfLockedUsers:
+		var itemUsername : Label = Label.new()
+		vBoxUser.add_child(itemUsername)
+		itemUsername.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		itemUsername.text = listOfLockedUsers[users]["SamAccountName"]
+		
+		var userItemColor : ColorRect = ColorRect.new()
+		itemUsername.add_child(userItemColor)
+		userItemColor.anchors_preset = 15 # PRESET_FULL_RECT
+		userItemColor.color = Color(0.5, 0.5, 0.5, 0.5)
+		userItemColor.show_behind_parent = true
+		
+		var changeUserItemSize := func() -> void:
+			userItemColor.size = itemUsername.size
+		itemUsername.resized.connect(changeUserItemSize)
+		
+		var itemPassword : Label = Label.new()
+		vBoxPass.add_child(itemPassword)
+		itemPassword.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		itemPassword.text = listOfLockedUsers[users]["PasswordExpired"]
+		
+		var passItemColor : ColorRect = ColorRect.new()
+		itemPassword.add_child(passItemColor)
+		passItemColor.anchors_preset = 15 # PRESET_FULL_RECT
+		passItemColor.color = Color(0.5, 0.5, 0.5, 0.5)
+		passItemColor.show_behind_parent = true
+		
+		var changePassItemSize := func() -> void:
+			passItemColor.size = itemPassword.size
+		itemPassword.resized.connect(changePassItemSize)
+		
+	_win.show()
 
 func IPCommandTabMenu(id : int) -> void:
 	match id:
