ff64011ccff46f3f6b477d874acebbceb0bb2095
MrOutsider <dk1ofakind@protonmail.com>
2023-06-07 08:35:32 -0700

    All but the apos working

--- scripts/WIN_AD_NEW.gd
+++ scripts/WIN_AD_NEW.gd
@@ -13,20 +13,15 @@ signal consoleMSG(text : String)
 @export var badge : LineEdit
 @export var user_position : MenuButton
 
-@onready var positions_file : String = "positions.cfg"
+@onready var positions_file : String = "config\\positions.cfg"
 
 var positions : Dictionary = {}
-
-var config : Dictionary = {"distinguishedName" : "", "description" : "", "title" : "", "department" : "",
-"company" : ""}
-var list_of_groups : Dictionary = {1 : "Patrol Global"}
+var config : Dictionary = {"distinguishedName" = ""}
+var list_of_groups : Dictionary = {1 : "Empty"}
 
 func _ready():
 	Load_Positions_Config()
-	
 	var user_pos_popup : PopupMenu = user_position.get_popup()
-	for n in positions.values():
-		user_pos_popup.add_item(n.Name)
 	user_pos_popup.id_pressed.connect(User_Position_Menu_Popup)
 
 func _on_close_requested() -> void:
@@ -46,6 +41,7 @@ func reset() -> void:
 	password.text = ""
 	badge.text = ""
 	user_position.text = "Position"
+	Load_Positions_Config()
 
 func consoleCommand(cmdArg : PackedStringArray) -> String:
 	var _consoleBuffer : Array = []
@@ -55,38 +51,39 @@ func consoleCommand(cmdArg : PackedStringArray) -> String:
 func User_Position_Menu_Popup(id : int) -> void:
 	for n in positions.keys():
 		if (str_to_var(n) == id):
-			user_position.text = positions[n].Name
+			user_position.text = positions[n].name
 			config.distinguishedName = positions[n].data.distinguishedName
 			config.description = positions[n].data.description
 			config.title = positions[n].data.title
 			config.department = positions[n].data.department
 			config.company = positions[n].data.company
+			list_of_groups = positions[n].data.groups
 			break
 
 func _on_button_pressed() -> void:
 	if (config.distinguishedName != ""):
 		var badge_text : String = ""
 		if (badge.text != ""):
-			badge_text = " #" + badge.text 
-
+			badge_text = " - #" + badge.text
+		
 		var _textBuffer : String = consoleCommand([
 			"New-ADUser",
-			"-Name", username.text.to_lower(),
-			"-SamAccountName", username.text.to_lower(),
-			"-UserPrincipalName", str(username.text + "@kcsdadmn.com").to_lower(),
-			"-GivenName", first_name.text,
-			"-Surname", last_name.text,
-			"-DisplayName", str(first_name.text + "' '" + last_name.text),
-			"-EmailAddress", str(first_name.text.left(1) + last_name.text + "@kcgov.us").to_lower(),
-			"-AccountPassword", str("(ConvertTo-SecureString -AsPlainText " + password.text + " -Force)"),
-			"-CannotChangePassword",  "$false",
+			"-Name", 				username.text.to_lower(),
+			"-SamAccountName", 		username.text.to_lower(),
+			"-UserPrincipalName", 	str(username.text + "@kcsdadmn.com").to_lower(),
+			"-GivenName", 			first_name.text,
+			"-Surname", 			last_name.text,
+			"-DisplayName", 		str(first_name.text + "' '" + last_name.text),
+			"-EmailAddress", 		str(first_name.text.left(1) + last_name.text + "@kcgov.us").to_lower(),
+			"-AccountPassword", 	str("(ConvertTo-SecureString -AsPlainText " + password.text + " -Force)"),
+			"-CannotChangePassword", "$false",
 			"-ChangePasswordAtLogon", "$true",
-			"-Description", str(config.description + badge_text).replace(" ", "' '"),
-			"-Title", config.title.replace(" ", "' '"),
-			"-Department", config.department.replace(" ", "' '"),
-			"-Company", config.company.replace(" ", "' '"),
-			"-Path", config.distinguishedName.replace(",", "','"),
-			"-Profilepath", str("\\\\kcsdadmn.com\\userfilespace\\UserProfiles\\" + username.text.to_lower()),
+			"-Description", 		str(config.description + badge_text).replace(" ", "' '"),
+			"-Title", 				config.title.replace(" ", "' '"),
+			"-Department", 			config.department.replace(" ", "' '"),
+			"-Company", 			config.company.replace(" ", "' '"),
+#			"-Path", 				config.distinguishedName.replace(",", "','"),
+			"-Profilepath", 		str("\\\\kcsdadmn.com\\userfilespace\\UserProfiles\\" + username.text.to_lower()),
 			"-Enabled $false"
 		])
 		
@@ -105,11 +102,12 @@ func Load_Positions_Config() -> void:
 		var error = json.parse(text)
 		if error == OK:
 			positions = json.data
-			if typeof(positions) == TYPE_ARRAY:
-				print(positions) # Prints array
-			else:
-				print("Unexpected data")
+			var user_pos_popup : PopupMenu = user_position.get_popup()
+			user_pos_popup.clear()
+			for n in positions.values():
+				user_pos_popup.add_item(n.name)
 		else:
-			print("JSON Parse Error: ", json.get_error_message(), " in ", text, " at line ", json.get_error_line())
+			consoleMSG.emit(str("ERROR::LOADING::"+ positions_file + " JSON Parse Error: ", json.get_error_message(), " in ", text, " at line ", json.get_error_line()))
+			print(str("ERROR::LOADING::"+ positions_file + " JSON Parse Error: ", json.get_error_message(), " in ", text, " at line ", json.get_error_line()))
 		json.is_queued_for_deletion()
 		file.is_queued_for_deletion()
