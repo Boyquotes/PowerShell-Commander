9288558b4f996d4b3a1f282527d07a7ff3f6c59b
MrOutsider <dk1ofakind@protonmail.com>
2023-06-06 17:53:39 -0700

    Dynamic Gen from Dict for Position Buttons

--- scripts/WIN_AD_NEW.gd
+++ scripts/WIN_AD_NEW.gd
@@ -13,11 +13,16 @@ signal consoleMSG(text : String)
 @export var badge : LineEdit
 @export var user_position : MenuButton
 
-var distinguishedName : String = ""
+var positions : Dictionary = {1 : "Deputy (PATROL)", 2 : "Deputy (JAIL)"}
+
+var config : Dictionary = {"distinguishedName" : "", "description" : "", "title" : "", "department" : "",
+"company" : ""}
 var list_of_groups : Dictionary = {1 : "Patrol Global"}
 
 func _ready():
 	var user_pos_popup : PopupMenu = user_position.get_popup()
+	for n in positions.keys():
+		user_pos_popup.add_item(positions[n])
 	user_pos_popup.id_pressed.connect(User_Position_Menu_Popup)
 
 func _on_close_requested() -> void:
@@ -26,7 +31,11 @@ func _on_close_requested() -> void:
 func reset() -> void:
 	position = Vector2i(50,100)
 	list_of_groups = {}
-	distinguishedName = ""
+	config.distinguishedName = ""
+	config.description = ""
+	config.title = ""
+	config.department = ""
+	config.company = ""
 	first_name.text = ""
 	last_name.text = ""
 	username.text = ""
@@ -40,40 +49,39 @@ func consoleCommand(cmdArg : PackedStringArray) -> String:
 	return _consoleBuffer[0]
 
 func User_Position_Menu_Popup(id : int) -> void:
-	match id:
-		0:
-			user_position.text = "Deputy (PATROL)"
-			distinguishedName = "OU=ISDEPT,DC=kcsdadmn,DC=com"
-		1:
-			user_position.text = "Deputy (JAIL)"
-			distinguishedName = "OU=ISDEPT,DC=kcsdadmn,DC=com"
-		2:
-			user_position.text = "Emergency Communications Officer (Dispatcher)"
-			distinguishedName = "OU=ISDEPT,DC=kcsdadmn,DC=com"
+	for n in positions.keys():
+		if (n - 1 == id):
+			user_position.text = positions[n]
+			config.distinguishedName = "OU=ISDEPT,DC=kcsdadmn,DC=com"
+			config.description = "PC/Network Specialist"
+			config.title = "PC/Network Specialist"
+			config.department = "IT"
+			config.company = "Kootenai County Sheriffs Office"
+			break
 
 func _on_button_pressed() -> void:
-	if (distinguishedName != ""):
+	if (config.distinguishedName != ""):
 		var badge_text : String = ""
 		if (badge.text != ""):
-			badge_text = "' '#" + badge.text 
+			badge_text = " #" + badge.text 
 
 		var _textBuffer : String = consoleCommand([
 			"New-ADUser",
 			"-Name", username.text.to_lower(),
 			"-SamAccountName", username.text.to_lower(),
-			"-UserPrincipalName", str(username.text.to_lower() + "@kcsdadmn.com"),
+			"-UserPrincipalName", str(username.text + "@kcsdadmn.com").to_lower(),
 			"-GivenName", first_name.text,
 			"-Surname", last_name.text,
 			"-DisplayName", str(first_name.text + "' '" + last_name.text),
-			"-EmailAddress", str(first_name.text.left(1).to_lower() + last_name.text.to_lower() + "@kcgov.us"),
+			"-EmailAddress", str(first_name.text.left(1) + last_name.text + "@kcgov.us").to_lower(),
 			"-AccountPassword", str("(ConvertTo-SecureString -AsPlainText " + password.text + " -Force)"),
 			"-CannotChangePassword",  "$false",
 			"-ChangePasswordAtLogon", "$true",
-			"-Description", str("PC/Network' 'Specialist" + badge_text),
-			"-Title", "PC/Network' 'Specialist",
-			"-Department", "IT",
-			"-Company", "Kootenai' 'County' 'Sheriffs' 'Office",
-			"-Path", distinguishedName.replace(",", "','"),
+			"-Description", str(config.description + badge_text).replace(" ", "' '"),
+			"-Title", config.title.replace(" ", "' '"),
+			"-Department", config.department.replace(" ", "' '"),
+			"-Company", config.company.replace(" ", "' '"),
+			"-Path", config.distinguishedName.replace(",", "','"),
 			"-Profilepath", str("\\\\kcsdadmn.com\\userfilespace\\UserProfiles\\" + username.text.to_lower()),
 			"-Enabled $false"
 		])
@@ -81,5 +89,5 @@ func _on_button_pressed() -> void:
 		consoleMSG.emit("User Creation Attempt. \n" + _textBuffer)
 		
 		for group in list_of_groups.values():
-			_textBuffer = consoleCommand(["Add-ADGroupMember", "-Identity", str(group).replace(" ", "\' \'"), "-Members", username.text])
-			consoleMSG.emit(username.text + " Added to " + str(group) + ".")
+			_textBuffer = consoleCommand(["Add-ADGroupMember", "-Identity", group.replace(" ", "\' \'"), "-Members", username.text])
+			consoleMSG.emit(username.text + " Add to " + group + ".")
